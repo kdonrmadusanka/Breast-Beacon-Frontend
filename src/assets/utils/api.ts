58f13ec7d193c1a80ffc9f9f22179241be733709
kdonrmadusanka <ravmkdon@gmail.com>
2025-06-28 10:55:36 +0530

    No important changes.

--- src/assets/utils/api.ts
+++ src/assets/utils/api.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "http://localhost:5000/api";
 
 export const registerPatient = async (data: {
   name: string;
