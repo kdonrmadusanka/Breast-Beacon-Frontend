5f2c9acdf890545cf5cdcd1544c855cceb6a4b27
kdonrmadusanka <ravmkdon@gmail.com>
2025-07-05 11:48:57 +0530

    Use barrel method to export ProgressBar.

--- src/assets/components/ui/Index.ts
+++ src/assets/components/ui/Index.ts
@@ -3,3 +3,4 @@ export { default as Input } from "./Input";
 export { default as Card } from "./Card";
 export { default as Model } from "./Model";
 export { default as LoadingSpinner } from "./LoadingSpinner";
+export { default as ProgressBar } from "./ProgressBar";
